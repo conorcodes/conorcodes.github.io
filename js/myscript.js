83f48d106c1ae26f472f7904c15ed19552fed29f
Conor Fitzgerald <cpfitz14@gmail.com>
2015-07-10 14:44:48 -0700

    moved webcamtex

--- js/myscript.js
+++ js/myscript.js
@@ -10,12 +10,7 @@ $('#svgupload').click(function(){
     pasteText.value = "";
 });
 
-////WEBCAM TEXTURE
-$('#webtex').click(function(){
-   var webtex = null;
-   webtex = new THREEx.WebcamTexture();
-   material.map = webtex.texture;
-    });
+
 		
 var size = $("#paperdiv").width();
 
@@ -215,7 +210,12 @@ var directionalLight = new THREE.DirectionalLight( 0xffeedd );
     
 var material = new THREE.MeshPhongMaterial( { map:textureagain,transparency:false, side:THREE.DoubleSide, opacity:1.0} );
 
-
+////WEBCAM TEXTURE
+$('#webtex').click(function(){
+   var webtex = null;
+   webtex = new THREEx.WebcamTexture();
+   material.map = webtex.texture;
+    });
 
 
 
