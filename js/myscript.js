b9aa5b04c05eff2b6360b06a1f5ad1fca14f08f2
Conor Fitzgerald <cpfitz14@gmail.com>
2015-07-13 15:58:39 -0700

    adding tools

--- js/myscript.js
+++ js/myscript.js
@@ -97,58 +97,258 @@ var segment, path;
 var movePath = false;
 
 
+///PAPER TOOLS
 
 
 
-var tool = new Tool();
 
 
+/////Pen Tool
 
-tool.onMouseDown = function (event) {
-    segment = path = null;
-    var hitResult = project.hitTest(event.point, hitOptions);
-    if (!hitResult) return;
+var penTool = new Tool();
 
-    if (event.modifiers.shift) {
-        if (hitResult.type == 'segment') {
-            hitResult.segment.remove();
-        }
+var path;
+var types = ['point', 'handleIn', 'handleOut'];
+function findHandle(point) {
+	for (var i = 0, l = path.segments.length; i < l; i++) {
+		for (var j = 0; j < 3; j++) {
+			var type = types[j];
+			var segment = path.segments[i];
+			var segmentPoint = type == 'point'
+					? segment.point
+					: segment.point + segment[type];
+			var distance = (point - segmentPoint).length;
+			if (distance < 3) {
+				return {
+					type: type,
+					segment: segment
+				};
+			}
+		}
+	}
+	return null;
+}
+
+var currentSegment, mode, type;
+penTool.onMouseDown = function(event) {
+	if (currentSegment)
+		currentSegment.selected = false;
+	mode = type = currentSegment = null;
+	
+	if (!path) {
+	    alert('making path')
+		path = new Path();
+		path.fillColor = new HSBColor(360 * Math.random(), 1, 1, 0.5);
+	}
+	alert(event.point);
+	
+	var result = findHandle(event.point);
+	if (result) {
+		currentSegment = result.segment;
+		type = result.type;
+		if (path.segments.length > 1 && result.type == 'point'
+				&& result.segment.index == 0) {
+			mode = 'close';
+			path.closed = true;
+			path.selected = false;
+			path = null;
+		}
+	}
+	
+	if (mode != 'close') {
+		mode = currentSegment ? 'move' : 'add';
+		if (!currentSegment)
+			currentSegment = path.add(event.point);
+		currentSegment.selected = true;
+	}
+}
+	
+penTool.onMouseDrag = function(event) {
+	if (mode == 'move' && type == 'point') {
+		currentSegment.point = event.point;
+	} else if (mode != 'close') {
+		var delta = event.delta.clone();
+		if (type == 'handleOut' || mode == 'add')
+			delta = -delta;
+		currentSegment.handleIn += delta;
+		currentSegment.handleOut -= delta;
+	}
+}
+	
+
+/////Selection Tool
+
+var selTool = new Tool();
+
+var selectionRectangle = null;
+var selectionRectangleScale=null;
+var selectionRectangleScaleNormalized=null;
+var selectionRectangleRotation=null;
+
+var segment, path, selectionRectangleSegment;
+var movePath = false;
+
+function initSelectionRectangle(path) {
+    if(selectionRectangle!=null)
+        selectionRectangle.remove();
+    var reset = path.rotation==0 && path.scaling.x==1 && path.scaling.y==1;
+    var bounds;
+    if(reset)
+    {
+        console.log('reset');
+        bounds = path.bounds;
+        path.pInitialBounds = path.bounds;
+    }
+    else
+    {
+        console.log('no reset');
+        bounds = path.pInitialBounds;
+    }
+    console.log('bounds: ' + bounds);
+    b = bounds.clone().expand(10,10);
+    
+    selectionRectangle = new Path.Rectangle(b);
+    selectionRectangle.pivot = selectionRectangle.position;
+    selectionRectangle.insert(2, new Point(b.center.x, b.top));
+    selectionRectangle.insert(2, new Point(b.center.x, b.top-25));
+    selectionRectangle.insert(2, new Point(b.center.x, b.top));
+    if(!reset)
+    {
+        selectionRectangle.position = path.bounds.center;
+        selectionRectangle.rotation = path.rotation;
+        selectionRectangle.scaling = path.scaling;
+    }
+
+    selectionRectangle.strokeWidth = 1;
+    selectionRectangle.strokeColor = 'blue';
+    selectionRectangle.name = "selection rectangle";
+    selectionRectangle.selected = true;
+    selectionRectangle.ppath = path;
+    selectionRectangle.ppath.pivot = selectionRectangle.pivot;
+}
+selTool.onMouseDown= function(event) {
+	segment = path = null;
+	var hitResult = project.hitTest(event.point, hitOptions);
+	if (!hitResult)
+		return;
+
+
+	if (hitResult) {
+        console.log(hitResult);
+		path = hitResult.item;
+		
+		if (hitResult.type == 'segment') {
+			if(selectionRectangle!=null && path.name == "selection rectangle")
+			{
+                console.log('selectionRectangle');
+                if(hitResult.segment.index >= 2 && hitResult.segment.index <= 4)
+                {
+                    console.log('rotation');
+                    selectionRectangleRotation = 0;
+                }
+                else
+                {
+                    console.log('scale');
+                    selectionRectangleScale = event.point.subtract(selectionRectangle.bounds.center).length/path.scaling.x;
+                }
+			}
+            else
+                segment = hitResult.segment;
+		} else if (hitResult.type == 'stroke' && path!=selectionRectangle) {
+			var location = hitResult.location;
+			//segment = path.insert(location.index + 1, event.point);
+			//path.smooth();
+		}
+		if((selectionRectangle==null || selectionRectangle.ppath!=path) && selectionRectangle!=path)
+		{
+            initSelectionRectangle(path);
+		}
+	}
+	else
+	{
+        if(selectionRectangle!=null)
+            selectionRectangle.remove();
+	}
+	movePath = hitResult.type == 'fill';
+	if (movePath)
+		project.activeLayer.addChild(hitResult.item);
+}
+
+selTool.onMouseMove= function(event) {
+	project.activeLayer.selected = false;
+	if (event.item)
+	{
+		event.item.selected = true;
+	}
+    if(selectionRectangle)
+        selectionRectangle.selected = true;
+}
+
+selTool.onMouseDrag = function(event) {
+	if (selectionRectangleScale!=null)
+	{
+        ratio = event.point.subtract(selectionRectangle.bounds.center).length/selectionRectangleScale;
+        scaling = new Point(ratio, ratio);
+        selectionRectangle.scaling = scaling;
+        selectionRectangle.ppath.scaling = scaling;
+        console.log('scaling: '+selectionRectangle.ppath);
         return;
-    }
+	}
+	else if(selectionRectangleRotation!=null)
+	{
+        console.log('rotation: '+selectionRectangle.ppath);
+        rotation = event.point.subtract(selectionRectangle.pivot).angle + 90;
+        selectionRectangle.ppath.rotation = rotation;
+        selectionRectangle.rotation = rotation;
+        return;
+	}
+	if (segment) {
+		//segment.point += event.delta;
+		//path.smooth();
+		//initSelectionRectangle(path);
+	} else if (path) {
+	    if (path!=selectionRectangle)
+	    {
+		    path.position.x += event.delta.x;
+		    path.position.y += event.delta.y;
 
-    if (hitResult) {
-        path = hitResult.item;
-        if (hitResult.type == 'segment') {
-            segment = hitResult.segment;
-        } else if (hitResult.type == 'stroke') {
-            var location = hitResult.location;
-            segment = path.insert(location.index + 1, event.point);
-            path.smooth();
-        }
-    }
-    movePath = hitResult.type == 'fill';
-    if (movePath) project.activeLayer.addChild(hitResult.item);
-};
+		    selectionRectangle.position.x += event.delta.x;
+		    selectionRectangle.position.y += event.delta.y;
 
-tool.onMouseMove = function onMouseMove(event) {
-    project.activeLayer.selected = false;
-    if (event.item) event.item.selected = true;
-};
+	    }
+	    else
+	    {
+		    selectionRectangle.position.x += event.delta.x;
+		    selectionRectangle.position.y += event.delta.y;
 
-tool.onMouseDrag = function onMouseDrag(event) {
-    if (segment) {
-        segment.point.x += event.delta.x;
-        segment.point.y += event.delta.y;
+		    selectionRectangle.ppath.position.x += event.delta.x;
+		    selectionRectangle.ppath.position.y += event.delta.y;
 
-        path.smooth();
-    } else if (path) {
-        path.position.x += event.delta.x;
-        path.position.y += event.delta.y;
+	    }
+	}
+}
 
+selTool.onMouseUp = function(event) {
+    selectionRectangleScale = null;
+    selectionRectangleRotation = null;
+}
 
+///Tool buttons
 
-    }
-};
+$('#penTool').click(function(){
+    removeSelRect();
+penTool.activate();
+});
+
+$('#selTool').click(function(){
+selTool.activate();
+});
+
+function removeSelRect(){
+     if(selectionRectangle!=null){
+            selectionRectangle.remove();
+	}
+}
 
 ///THIS IS THREE JS STUFF////
 
